ba1f7ebe120941a7a30edae5504bd57e2e9b5fed
Riza Pranata <corodotc@gmail.com>
2019-11-06 15:50:03 +0700

    repeatFun2

---
+++ repeatFun.js
@@ -0,0 +1,8 @@
+function repeat(operation, num) {
+    var num_array = new Array(num);
+    for (var i = 0; i < num_array.length; i++) {
+        return operation(num);
+    }
+}
+
+module.exports = repeat 
