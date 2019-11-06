3e93a21f4f5cb4fbfee6a9a303f6889fed408ec6
Riza Pranata <corodotc@gmail.com>
2019-11-06 16:07:47 +0700

    upload reduceCount & repeatFun

--- repeatFun.js
+++ repeatFun.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function repeat(operation, num) {
     var num_array = new Array(num);
     for (var i = 0; i < num_array.length; i++) {
@@ -7,13 +7,3 @@ function repeat(operation, num) {
 }
 
 module.exports = repeat 
-=======
-function repeat(operation, num) {
-    var num_array = new Array(num);
-    for (var i = 0; i < num_array.length; i++) {
-        return operation(num);
-    }
-}
-
-module.exports = repeat 
->>>>>>> e7664792060334812ee70d156e8cb969792aa870
